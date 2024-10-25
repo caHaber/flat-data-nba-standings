7460384b9e96711518c07f48922a0db4dc7e4f03
Casey Aaron Haber <haber.casey@gmail.com>
2024-10-25 14:08:26 -0700

    Update postprocess.ts

--- postprocess.ts
+++ postprocess.ts
@@ -1,10 +1,10 @@
 // Is this the cause of the failures ?
 // import "https://deno.land/x/flat@0.0.10/mod.ts";
-import { removeFile } from "https://deno.land/x/flat/mod.ts";
+// import { removeFile } from "https://deno.land/x/flat/mod.ts";
 
 import { table_to_csv } from "./table_csv.ts";
 
-const filename = Deno.args[0]; // Same name as downloaded_filename `const filename = 'btc-price.json';`
+// const filename = Deno.args[0]; // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const html = await Deno.readTextFile('NBA_2025.html');
 
 const EAST_standings: any = table_to_csv("confs_standings_E", html);
@@ -14,4 +14,4 @@ console.log(EAST_standings, WEST_standings);
 
 await Deno.writeTextFile("EAST_standings.csv", EAST_standings);
 await Deno.writeTextFile("WEST_standings.csv", WEST_standings);
-await removeFile(filename);
+// await removeFile(filename);
